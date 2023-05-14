b3170d63eed6b63888ccbff4ee39cc22d6a4a7ef
mateocarciu <82208043+mateocarciu@users.noreply.github.com>
2023-05-14 13:02:21 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,86 @@
 
-
+// Fonction pour créer un cookie
+function createCookie(name, value, days) {
+    let expires;
+    if (days) {
+      let date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
+    }
+    else {
+      expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+  
+  // Fonction pour lire la valeur d'un cookie
+  function readCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i=0;i < ca.length;i++) {
+      let c = ca[i];
+      while (c.charAt(0)==' ') {
+        c = c.substring(1,c.length);
+      }
+      if (c.indexOf(nameEQ) == 0) {
+        return c.substring(nameEQ.length,c.length);
+      }
+    }
+    return null;
+  }
+  
+  // Fonction pour supprimer un cookie
+  function eraseCookie(name) {
+    createCookie(name,"",-1);
+  }
+  
+  // Fonction pour vérifier si un cookie existe déjà
+  function checkCookie(name) {
+    let cookieValue = readCookie(name);
+    if (cookieValue != null && cookieValue != "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  // Fonction pour afficher la popup et gérer les cookies
+  function manageCookies() {
+    let popup = document.createElement('div');
+    popup.style.position = 'fixed';
+    popup.style.bottom = '0';
+    popup.style.left = '0';
+    popup.style.width = '100%';
+    popup.style.padding = '20px';
+    popup.style.backgroundColor = '#fff';
+    popup.style.boxShadow = '0px -1px 5px rgba(0,0,0,0.2)';
+    popup.innerHTML = '<p>Notre site utilise des cookies pour améliorer votre expérience de navigation. En continuant à utiliser notre site, vous acceptez notre utilisation des cookies.</p><button id="accept" class="cookie-button accept-button">Accepter</button><button id="decline" class="cookie-button decline-button">Refuser</button>';
+    document.body.appendChild(popup);
+  
+  
+    // Si le cookie "accepted_cookies" existe, masquer la popup
+    if (checkCookie('accepted_cookies')) {
+      popup.style.display = 'none';
+    }
+  
+    // Gérer l'acceptation des cookies
+    let acceptBtn = document.getElementById('accept');
+    acceptBtn.addEventListener('click', function() {
+      createCookie('accepted_cookies', 'true', 30);
+      popup.style.display = 'none';
+    });
+  
+    // Gérer le refus des cookies
+    let declineBtn = document.getElementById('decline');
+    declineBtn.addEventListener('click', function() {
+      eraseCookie('accepted_cookies');
+      popup.style.display = 'none';
+    });
+  }
+  
+  // Appeler la fonction pour afficher la popup
+  manageCookies();
+  
 // function checkform(theform){
 //     var why = "";
     
